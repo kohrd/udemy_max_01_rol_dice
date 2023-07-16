d7e1eb93ebae5ea71de22144434467829e58bc15
kohrd <konrad.janeczek@protonmail.com>
2023-07-16 22:18:47 +0200

    film 46  1min8sek

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -37,10 +37,14 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
+
         // child: MyTextStyle("Flutter really sucks!"),
         child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset('assets/images/dice-2.png', width: 200),
+            SizedBox(height: 200,),
             TextButton(
               onPressed: rollDice,
               child: const Text('Rzuć kostką!',
