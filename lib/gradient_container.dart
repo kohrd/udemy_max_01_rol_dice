2b2160f13729ea956208f23294f4643c95b44e33
kohrd <konrad.janeczek@protonmail.com>
2023-07-16 17:13:34 +0200

    45

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,40 +1,56 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:roll_dice_app/styled_text.dart';
 
 var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
 
+class GradientContainer extends StatelessWidget {
+  // konstruktor
+  const GradientContainer(
+      {required this.color1, required this.color2, super.key});
 
+  //alternatywny konstruktor
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
 
-class GradientContainer extends StatelessWidget{
-  const GradientContainer({required this.color1, required this.color2, super.key});
-
- final Color color1;
+  final Color color1;
   final Color color2;
 
+
+  void rollDice(){
+    //...
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration:  BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [color1, color2],
-            // colors: const [ Color.fromARGB(200, 45, 89, 102),
-            //   Color.fromARGB(1, 1, 1, 2)
-            // ],
-            begin: startAlignment,
-            end: endAlignment,
-
+          // colors: const [ Color.fromARGB(200, 45, 89, 102),
+          //   Color.fromARGB(1, 1, 1, 2)
+          // ],
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
-      child: const Center(
-          child: MyTextStyle("Flutter really sucks!"),
+      child: Center(
+        // child: MyTextStyle("Flutter really sucks!"),
+        child: Column(
+          children: [
+            Image.asset('assets/images/dice-2.png', width: 200),
+            TextButton(
+              onPressed: rollDice,
+              child: const Text('Rzuć kostką!',
+                  style: TextStyle(color: Colors.white54, fontSize: 40)),
+            ),
+          ],
+        ),
+        // Image.assets - po krope jest nazwa alternatywnego konstrukora klasy Image
       ),
     );
     throw UnimplementedError();
   }
-
-
 }
